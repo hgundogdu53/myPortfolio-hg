23e8ae29488577a1fca40a303ccaabaa8f4c3b11
Huseyin Gundogdu <38477613+hgundogdu53@users.noreply.github.com>
2019-07-14 17:59:55 -0700

    image sources updated.

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ function clickOnAbout() {
 function clickOnProjects() {
     $('#projects').on('click', event => {
         $('.main').hide();
-        $('.js-top').html(`<div class="js-project"><h1>Breaking Bad Quiz App</h1><div class="logos"><img class="first-logo" alt="html logo" src="/Users/hsy-53/projects/html.png"/><img class="second-logo" alt="css logo" src="/Users/hsy-53/projects/csslogo-2582747_960_720.png"/>
-        <img class="third-logo" alt="js logo" src="/Users/hsy-53/projects/js.png"/><img alt="jquery-logo" class="last-logo" src="/Users/hsy-53/projects/jquery-logo.jpg"/></div>
-        <img class="intro-quiz" src="/Users/hsy-53/projects/Screen Shot 2019-07-13 at 8.10.53 PM.png" alt="quiz app intro image"/>
+        $('.js-top').html(`<div class="js-project"><h1>Breaking Bad Quiz App</h1><div class="logos"><img class="first-logo" alt="html logo" src="html.png"/><img class="second-logo" alt="css logo" src="csslogo-2582747_960_720.png"/>
+        <img class="third-logo" alt="js logo" src="js.png"/><img alt="jquery-logo" class="last-logo" src="jquery-logo.jpg"/></div>
+        <img class="intro-quiz" src="Screen Shot 2019-07-13 at 8.10.53 PM.png" alt="quiz app intro image"/>
         <p>Test your 'Breaking Bad' knowledge with this quiz app. 
         It includes 10 questions and four choices per question. If you know the question, you will get '1' score for every question. Don't worry, if you make a wrong, correct answer will show up on the screen.
         End of the quiz, get your score and you can restart it, if you want.</p><div class="targets">
