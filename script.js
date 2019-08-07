6841b47fa2eab60280871cde3c0c7d741ca98f20
Huseyin Gundogdu <hgundogdu53@gmail.com>
2019-08-06 20:59:04 -0700

    second project added

--- script.js
+++ script.js
@@ -31,6 +31,14 @@ function clickOnProjects() {
         End of the quiz, get your score and you can restart it, if you want.</p><div class="targets">
         <a target="_blank" href="https://github.com/hgundogdu53/breakingBad-quizApp">Repo</a>
         <a target="_blank" href="https://hgundogdu53.github.io/breakingBad-quizApp/">Live</a></div>
+        </div><div class="js-project second"><h1>Okie-Dokie-Recipe</h1><div class="logos"><img class="first-logo" alt="html logo" src="/Users/hsy-53/projects/html.png"/><img class="second-logo" alt="css logo" src="/Users/hsy-53/projects/csslogo-2582747_960_720.png"/>
+        <img class="third-logo" alt="js logo" src="/Users/hsy-53/projects/js.png"/><img alt="jquery-logo" class="last-logo" src="/Users/hsy-53/projects/jquery-logo.jpg"/></div>
+        <img class="intro-quiz second" src="/Users/hsy-53/projects/portfolio/Screen Shot 2019-08-06 at 6.42.15 PM.png" alt="quiz app intro image"/>
+        <p>This apps allows to reach food and beverage recipes. 
+        If you want to decide cook something or prepare cocktails, you can make a search.
+        As a result, you will get recipe and video lists.</p><div class="targets">
+        <a target="_blank" href="https://github.com/hgundogdu53/Okie-Dokie-Recipe">Repo</a>
+        <a target="_blank" href="https://hgundogdu53.github.io/Okie-Dokie-Recipe/">Live</a></div>
         </div>`);
     });
 }
